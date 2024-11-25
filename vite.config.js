2a43a457b93ddcbbfb77a534ad41bea3115b4a8e
Ricardo <theartisanmidia@gmail.com>
2024-11-24 23:07:30 -0300

    otro

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bps-frontend/',
+  base: '/bps_frontend/',
   build: {
     outDir: 'docs', // Si estás usando 'docs' para GitHub Pages
   },
